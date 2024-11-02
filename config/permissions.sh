7aec23e25879c6162b779e75ec3f2863701e4876
kurocifer <draculemihawk232@gmail.com>
2024-11-02 21:55:50 +0100

    Add the different permissions

---
+++ config/permissions.sh
@@ -0,0 +1,9 @@
+# Some permissions that can be granted.
+
+DEFAULT_PERMISSIONS=755	# Owner has full permissions; group and others can read/execute.
+PRIVATE_ACCESS=700		# Full permissions for owner; no acces for others.
+GROUP_COLLABORATION=750	# Owner has full permissions; group members can read/execute; no access for others
+FULL_GROUP_ACCESS=770	# Owner and group members have full permissions; no access for others
+OPEN_ACCESS=777			# Full permissions for everyone
+READ_ONLY_FILES=644		# Owner can read/write; group and others can only read.
+PRIVATE_FILES=600		# Owner can read/write; no access for others.
