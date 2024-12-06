4d10d5fa36ae563140ec6d761cfddd51fef97041
kurocifer <draculemihawk232@gmail.com>
2024-12-06 16:49:42 +0100

    Add loop to add users in various groups

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -13,6 +13,19 @@ print_permissions() {
 	echo "6 - Private Files: "$PRIVATE_FILES""
 }
 
+group_exist() {
+	getent group "$1" > /dev/null 2>&1
+}
+
+create_group() {
+	groupadd "$1"
+	echo "Group '$1' created successfully"
+}
+
+add_user_to_group() {
+	usermod -aG "$1" "$2"
+	echo "User '$2" successfully added to group $1"
+}
 
 LIST_OF_USER_PERMISSIONS=("$DEFAULT_PERMISSIONS" "$PRIVATE_ACCESS" "$GROUP_COLLABORATION" "$FULL_GROUP_ACCESS" "$OPEN_ACCESS" "$READ_ONLY_FILES" "$PRIVATE_FILES")
 
@@ -43,8 +56,6 @@ printf "\n\n----- Setting up New User Onboarding -----\n\n"
 useradd -m -d "$USER_HOME" -s /bin/bash "$USERNAME"
 printf "\nUser $USERNAME created successfully.\n"
 
-
-
 # Set user permissions
 printf "\nSelect from the list below the permissions to be assigned to the user (0 - 6)\n\n"
 print_permissions
@@ -63,14 +74,36 @@ printf "\nPermissions "$USER_PERMISSIONS" have been set for $USER_HOME.\n"
 
 read -p "Do you want to set a password for this user ? (Y/N): " choice
 
-if [[ "$choice" -eq "Y" || "$choice" -eq "y" ]]; then
+if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
 	passwd $USERNAME
+	printf "\n password for user '$USERNAME' set successfully\n"
+fi
 
 
 # Add user to groups
-usermod -aG sudo "$USERNAME" # Add to sudo group
-printf "\n$USERNAME added to default groups.\n"
+# usermod -aG sudo "$USERNAME" # Add to sudo group
+# printf "\n$USERNAME added to default groups.\n"
 
+read -p "Do you want to add this user to groups ? (Y/N): " choice
+
+if [[ "$choice == "y" || "$choice" == "Y" ]]; then
+	while True
+	do
+		read -p "group name: " group_name
+
+		if group_exist $group_name; then
+			add_user_to_group $group_name $USERNAME
+		else
+			read -p "'$group_name' does not exist create it ? (Y/N): " create_group_prompt
+		fi
+		if [[ "$create_group_prompt" == "y" || "$create_group_prompt" == "Y" ]]; then
+			create_group $group_name
+			add_user_to_group $group_name $USERNAME
+
+		read -p "add user to another group ? (Y/N): " choice
+		if [[ "$choice" != "Y" || "$choice" != "y" ]]; then
+			break
+	done
 
 # Run ssh setup script
 ./setup_ssh.sh "$USERNAME"
